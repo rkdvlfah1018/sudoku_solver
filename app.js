053604f587ea13c2f7e047df302e303280222281
William Kang <wkang2020@gmail.com>
2023-05-19 16:18:30 -0500

    linked playing board with internal board for calculation

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 9; i++){
 for (let j = 0; j < 9; j++){
     $('.row').append("<td></td>")
     for (let k = 0; k < 9; k++){
-        board[j].push(-1);
+        board[j].push(-1); //empty cells are -1
     }
 }
 
@@ -16,6 +16,10 @@ const playground = document.getElementById("board")
 playground.addEventListener('click', function(e){
     var target = e.target;
     if (target.tagName === 'TD'){
+        // find row and column indexes
+        var rowIndex = target.parentNode.rowIndex;
+        var columnIndex = target.cellIndex;
+
         var currentText = target.textContent;
         target.innerHTML = '<input type="text" maxlength = "1" value="' + currentText + '">';
         var input = target.querySelector('input');
@@ -24,6 +28,12 @@ playground.addEventListener('click', function(e){
         input.addEventListener('input', function() {
             var newValue = this.value.replace(/[^1-9]/g, ''); // Remove any non-numeric values from 1 to 9
             this.value = newValue;
+            if (newValue == ""){
+                board[rowIndex][columnIndex] = -1
+            }
+            else{
+                board[rowIndex][columnIndex] = newValue
+            }
         });
 
         input.addEventListener('blur', function() {
