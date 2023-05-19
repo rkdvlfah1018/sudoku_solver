af6e69a73082247f9fb774c55404aab8f3234196
William Kang <wkang2020@gmail.com>
2023-05-19 16:47:01 -0500

    added solving algorithm

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ playground.addEventListener('click', function(e){
                 board[rowIndex][columnIndex] = -1
             }
             else{
-                board[rowIndex][columnIndex] = newValue
+                board[rowIndex][columnIndex] = Number(newValue)
             }
         });
 
@@ -42,3 +42,67 @@ playground.addEventListener('click', function(e){
         });
     }
 })
+
+var solveButton = document.getElementById("solve");
+
+solveButton.addEventListener('click', function(){
+    if(solve(board, 0, 0)){
+        console.log(board)
+    }
+    else{
+        alert("No solution")
+    }
+});
+
+function solve(grid, row, col){
+    if (row == 8 && col == 9){
+        return true;
+    }
+
+    if (col == 9){
+        row++;
+        col = 0;
+    }
+
+    if (grid[row][col] != -1){
+        return solve(grid, row, col + 1);
+    }
+
+    for(let num = 1; num < 10; num++){
+        if (isSafe(grid, row, col, num)){
+            grid[row][col] = num;
+
+            if(solve(grid, row, col + 1)){
+                return true;
+            }
+        }
+
+        grid[row][col] = -1
+    }
+    return false
+}
+
+function isSafe(grid, row, col, num){
+    for (let x = 0; x <= 8; x++){
+        if (grid[row][x] == num){
+            return false;
+        }
+    }
+
+    for(let x = 0; x <= 8; x++){
+        if (grid[x][col] == num){
+            return false;
+        }
+    }
+    
+    let startRow = row - row % 3,
+        startCol = col - col % 3;
+         
+    for(let i = 0; i < 3; i++)
+        for(let j = 0; j < 3; j++)
+            if (grid[i + startRow][j + startCol] == num)
+                return false;
+ 
+    return true;
+ 
+}
